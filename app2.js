bba047e3c614a2a6dfe97e6cdde5e07b67bb4632
Parhamkhd <31856253+Parhamkhd@users.noreply.github.com>
2020-08-25 20:45:39 +0430

    fixed clock

--- app2.js
+++ app2.js
@@ -1,2 +1,28 @@
+var countDownDate = new Date("Aug 28, 2020 15:37:25").getTime();
 
+var x = setInterval(function () {
 
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.querySelector("div#seconds1.timebox").innerHTML = seconds;
+    document.querySelector("div#minutes1.timebox").innerHTML = minutes;
+    document.querySelector("div#hours1.timebox").innerHTML = hours;
+    document.querySelector("div#days1.timebox").innerHTML = days;
+
+    // If the count down is over, write some text 
+    // if (distance < 0) {
+    //   clearInterval(x);
+    //   document.getElementById("demo").innerHTML = "EXPIRED";
+    // }
+}, 1000);
